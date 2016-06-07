d529575b5e3ea0cfa1580a8a203c8a401bb2ceb4
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2016-06-07 17:14:09 +0900

    fix

--- infrataster/spec/sample_spec.rb
+++ infrataster/spec/sample_spec.rb
@@ -1,7 +1,6 @@
 describe server(:ci) do
   describe http('http://spechost') do
     it do
-      expect(response.code).to eq('204')
       expect(response.headers['X-Frame-Options']).to eq('SAMEORIGIN')
       expect(response.headers['X-XSS-Protection']).to eq('1; mode=block')
       expect(response.headers['X-Content-Type-Options']).to eq('nosniff')
