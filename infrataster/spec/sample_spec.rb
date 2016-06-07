cdee72783b14ea5b8ede5adf7e810d0c97844617
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2016-06-07 12:40:32 +0900

    add sample

---
+++ infrataster/spec/sample_spec.rb
@@ -0,0 +1,10 @@
+describe server(:ci) do
+  describe http("http://#{$TARGET_HOST}") do
+    it "responds content including 'Hello Sinatra'" do
+      expect(response.body).to include('Hello Sinatra')
+    end
+    it "responds as 'text/html'" do
+      expect(response.headers['content-type']).to eq("text/html")
+    end
+  end
+end
